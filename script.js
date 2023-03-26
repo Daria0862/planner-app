0dd390df4f319b3790fbddb85b3a3d03364c8445
Tehmeena Chattha <119902254+Daria0862@users.noreply.github.com>
2023-03-26 15:52:50 +0100

    updated js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
-    $("currentDay").text(moment().format("dddd Do MMMM"));
-})
+$(document).ready(function () {
+    $("#currentDay").text(moment().format("dddd Do MMMM"));
+
+    var savedEvents = JSON.parse(localStorage.getItem("events")) || [];
+
+
+    $(".saveBtn").on("click", function () {
+        var eventText = $(this).siblings(".description").val();
+        var eventTime = $(this).parent().attr("id");
+        savedEvents.push({ time: eventTime, text: eventText });
+        localStorage.setItem("events", JSON.stringify(savedEvents));
+    });
+
+    for (var i = 0; i < savedEvents.length; i++) {
+        $("#" + savedEvents[i].time).children(".description").val(savedEvents[i].text);
+    }
+
+});
