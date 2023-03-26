6f928c18383ac9a092ffdfccb076e9f6171983ae
Tehmeena Chattha <119902254+Daria0862@users.noreply.github.com>
2023-03-26 14:50:04 +0100

    define hours

---
+++ script.js
@@ -0,0 +1,11 @@
+const hours = [
+    {hour: "09", meridiem: "00"},
+    {hour: "10", meridiem: "00"},
+    {hour: "11", meridiem: "00"},
+    {hour: "12", meridiem: "00"},
+    {hour: "13", meridiem: "00"},
+    {hour: "14", meridiem: "00"},
+    {hour: "15", meridiem: "00"},
+    {hour: "16", meridiem: "00"},
+    {hour: "17", meridiem: "00"},
+]
